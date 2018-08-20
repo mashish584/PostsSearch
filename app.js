08fed009c8260f4002a6d9c3602232e9438da408
Ashish Mehra <sdashish.am@gmail.com>
2018-08-20 14:46:21 +0530

    Base Project Setup

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+require("dotenv").config({ path: "secret.env" });
+
+const app = express();
+
+// setting view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// log requests
+app.use(logger("dev"));
+
+// use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// use static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/search", (req, res) => {
+    res.render("index", { title: "Post Search" });
+});
+
+app.use((req, res, next) => {
+    const error = new Error("NOT FOUND");
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.send(`${err.message} ${[err.status]}`);
+});
+
+//setting port
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), err => {
+    const message = err
+        ? "Problem while running the application server"
+        : `Server is running on port ${app.get("port")}`;
+    console.log(message);
+});
